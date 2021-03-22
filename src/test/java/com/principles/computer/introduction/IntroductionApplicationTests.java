d492c5b0557879c7349fbe2322742bdb77ed7836
Administrator <2435029569@qq.com>
2021-03-23 00:20:53 +0800

    计算机原理，操作系统，网络项目创建

---
+++ src/test/java/com/principles/computer/introduction/IntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.principles.computer.introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
